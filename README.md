05c68950ab28c5c6e36a2c1c31cb2134fbbac19a
João Malaca Fróis <joaolfrois@gmail.com>
2023-07-12 10:41:36 +0100

    Fixed duplicated data

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# pokedex
+# pokedex# pokedex
+
+
+## Project Setup
+
+```sh
+npm install
+```
+
+### Compile and Hot-Reload for Development
+
+```sh
+npm run dev
+```
+
+### Type-Check, Compile and Minify for Production
+
+```sh
+npm run build
+```
+
+### Lint with [ESLint](https://eslint.org/)
+
+```sh
+npm run lint
+```
--- src/modules/Pokedex/store/pokemonStore.ts
+++ src/modules/Pokedex/store/pokemonStore.ts
@@ -1,37 +1,37 @@
-import { defineStore } from 'pinia';
-import { fetchPokemonsList, getPokemonInfo, searchPokemon } from '../api/pokeapi';
+import { defineStore } from 'pinia'
+import { fetchPokemonsList, getPokemonInfo, searchPokemon } from '../api/pokeapi'
 
 interface Pokemon {
-  id: number;
-  name: string;
-  location_area_encounters: string;
-  image: string;
-  stats: Stat;
-  types: Type[];
+  id: number
+  name: string
+  location_area_encounters: string
+  image: string
+  stats: Stat
+  types: Type[]
 }
 
 interface Stat {
-  hp: number;
-  attack: number;
-  defense: number;
-  special_attack: number;
-  special_defense: number;
-  speed: number;
+  hp: number
+  attack: number
+  defense: number
+  special_attack: number
+  special_defense: number
+  speed: number
 }
 
 interface Type {
-  name: string;
+  name: string
 }
 
 interface PokemonState {
-  pokemons: Pokemon[];
-  isLoading: boolean;
-  isFiltered: boolean;
-  selectedPokemon: Pokemon | null;
-  searchQuery: string;
-  selectedTypes: string[];
-  error: boolean;
-  searchedPokemon: Pokemon | null;
+  pokemons: Pokemon[]
+  isLoading: boolean
+  isFiltered: boolean
+  selectedPokemon: Pokemon | null
+  searchQuery: string
+  selectedTypes: string[]
+  error: boolean
+  searchedPokemon: Pokemon | null
 }
 
 function mapStats(stats: any[]): Stat {
@@ -39,27 +39,27 @@ function mapStats(stats: any[]): Stat {
     (acc: Partial<Stat>, stat) => {
       switch (stat.stat.name) {
         case 'hp':
-          acc.hp = stat.base_stat || 0;
-          break;
+          acc.hp = stat.base_stat || 0
+          break
         case 'attack':
-          acc.attack = stat.base_stat || 0;
-          break;
+          acc.attack = stat.base_stat || 0
+          break
         case 'defense':
-          acc.defense = stat.base_stat || 0;
-          break;
+          acc.defense = stat.base_stat || 0
+          break
         case 'special-attack':
-          acc.special_attack = stat.base_stat || 0;
-          break;
+          acc.special_attack = stat.base_stat || 0
+          break
         case 'special-defense':
-          acc.special_defense = stat.base_stat || 0;
-          break;
+          acc.special_defense = stat.base_stat || 0
+          break
         case 'speed':
-          acc.speed = stat.base_stat || 0;
-          break;
+          acc.speed = stat.base_stat || 0
+          break
         default:
-          break;
+          break
       }
-      return acc;
+      return acc
     },
     {
       hp: 0,
@@ -67,9 +67,9 @@ function mapStats(stats: any[]): Stat {
       defense: 0,
       special_attack: 0,
       special_defense: 0,
-      speed: 0,
+      speed: 0
     } as Partial<Stat>
-  ) as Stat;
+  ) as Stat
 }
 
 export const usePokemonStore = defineStore('pokemon', {
@@ -86,96 +86,100 @@ export const usePokemonStore = defineStore('pokemon', {
 
   getters: {
     getSortedPokemons(state): Pokemon[] {
-      return state.pokemons.slice().sort((a, b) => a.id - b.id);
+      return state.pokemons.slice().sort((a, b) => a.id - b.id)
     },
     getLoadingState(state): boolean {
-      return state.isLoading;
+      return state.isLoading
     },
     getError(state): boolean {
-      return state.error;
+      return state.error
     },
     getIsFiltered(state): boolean {
-      return state.isFiltered;
+      return state.isFiltered
     },
-    getSearchedPokemon(state): Pokemon | null{
-      return state.searchedPokemon;
+    getSearchedPokemon(state): Pokemon | null {
+      return state.searchedPokemon
     }
-
   },
 
   actions: {
     async fetchPokemons() {
-      const offset = this.$state.pokemons.length;
+      const offset = this.$state.pokemons.length
       try {
-        const fetchedData = await fetchPokemonsList(offset);
-        const pokemonsList = fetchedData.results;
-        const pokemonPromises = pokemonsList.map((pokemon) => getPokemonInfo(pokemon.url));
-        const pokemonInfos = await Promise.all(pokemonPromises);
+        const fetchedData = await fetchPokemonsList(offset)
+        const pokemonsList = fetchedData.results
+        const pokemonPromises = pokemonsList.map((pokemon) => getPokemonInfo(pokemon.url))
+        const pokemonInfos = await Promise.all(pokemonPromises)
         const pokemons = pokemonInfos.map((pokemonInfo) => ({
           id: pokemonInfo.id,
           name: pokemonInfo.forms[0].name,
           image: pokemonInfo.sprites.front_default,
           types: pokemonInfo.types.map((typeData) => ({
-            name: typeData.type.name,
-          })),
-          location_area_encounters: '',
-          stats: mapStats(pokemonInfo.stats),
-        }));
-        this.$state.pokemons = pokemons;
-      } catch (error) {
-        console.error('Failed to fetch pokemons:', error);
-      }
-    },
-    async fetchMorePokemons() {
-      const offset = this.$state.pokemons.length;
-      try {
-        const fetchedData = await fetchPokemonsList(offset);
-        const pokemonsList = fetchedData.results;
-        const pokemonPromises = pokemonsList.map((pokemon) => getPokemonInfo(pokemon.url));
-        const pokemonInfos = await Promise.all(pokemonPromises);
-        const pokemons = pokemonInfos.map((pokemonInfo) => ({
-          id: pokemonInfo.id,
-          name: pokemonInfo.forms[0].name,
-          image: pokemonInfo.sprites.front_default,
-          types: pokemonInfo.types.map((typeData) => ({
-            name: typeData.type.name,
+            name: typeData.type.name
           })),
           location_area_encounters: '',
           stats: mapStats(pokemonInfo.stats)
-        }));
-        this.$state.pokemons = [...this.$state.pokemons, ...pokemons];
+        }))
+        this.$state.pokemons = pokemons
       } catch (error) {
-        console.error('Failed to fetch more pokemons:', error);
+        console.error('Failed to fetch pokemons:', error)
+      }
+    },
+    async fetchMorePokemons() {
+      if (!this.$state.isLoading) {
+        const offset = this.$state.pokemons.length
+        try {
+          this.$state.isLoading = true
+          const fetchedData = await fetchPokemonsList(offset)
+          const pokemonsList = fetchedData.results
+          const pokemonPromises = pokemonsList.map((pokemon) => getPokemonInfo(pokemon.url))
+          const pokemonInfos = await Promise.all(pokemonPromises)
+          const pokemons = pokemonInfos.map((pokemonInfo) => ({
+            id: pokemonInfo.id,
+            name: pokemonInfo.forms[0].name,
+            image: pokemonInfo.sprites.front_default,
+            types: pokemonInfo.types.map((typeData) => ({
+              name: typeData.type.name
+            })),
+            location_area_encounters: '',
+            stats: mapStats(pokemonInfo.stats)
+          }))
+          this.$state.pokemons = [...this.$state.pokemons, ...pokemons]
+        } catch (error) {
+          console.error('Failed to fetch more pokemons:', error)
+        } finally {
+          this.$state.isLoading = false
+        }
       }
     },
     async fetchPokemonByName(query: string) {
       try {
-        const fetchedData = await searchPokemon(query.toLowerCase());
+        const fetchedData = await searchPokemon(query.toLowerCase())
         const pokemon = {
           id: fetchedData.id,
           name: fetchedData.forms[0].name,
           image: fetchedData.sprites.front_default,
           types: fetchedData.types.map((typeData) => ({
-            name: typeData.type.name,
+            name: typeData.type.name
           })),
           location_area_encounters: '',
           stats: mapStats(fetchedData.stats)
-        };
-        this.$state.searchedPokemon = pokemon;
-        console.log( this.$state.searchedPokemon)
+        }
+        this.$state.searchedPokemon = pokemon
+        console.log(this.$state.searchedPokemon)
       } catch (error) {
         this.$state.searchedPokemon = null
-        console.error('Failed to search pokemon by name:', error);
+        console.error('Failed to search pokemon by name:', error)
       }
     },
     sortPokemons() {
-      this.$state.pokemons = [...this.$state.pokemons].sort((a, b) => a.id - b.id);
+      this.$state.pokemons = [...this.$state.pokemons].sort((a, b) => a.id - b.id)
     },
     changeIsFiltered() {
-      this.$state.isFiltered = !this.$state.isFiltered;
+      this.$state.isFiltered = !this.$state.isFiltered
     },
     clearSearchedPokemon() {
-      this.$state.searchedPokemon = null;
+      this.$state.searchedPokemon = null
     }
   }
-});
+})
